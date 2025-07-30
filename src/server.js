e865c7ae2e34f166ed878c70653597413d68ff3b
Mujahid2000 <developermujahid2001@gmail.com>
2025-07-30 22:35:34 +0600

    aremov

--- src/server.js
+++ src/server.js
@@ -7,14 +7,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const dbPath = path.join(__dirname, 'dua_main.sqlite');
-const db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error('Could not open database', err);
-  } else {
-    console.log('Connected to SQLite database');
-  }
-});
+// const dbPath = path.join(__dirname, 'dua_main.sqlite');
+// const db = new sqlite3.Database(dbPath, (err) => {
+//   if (err) {
+//     console.error('Could not open database', err);
+//   } else {
+//     console.log('Connected to SQLite database');
+//   }
+// });
 
 // Middleware
 app.use(cors());
@@ -25,26 +25,26 @@ app.get('/', (req, res) => {
   res.send('Express server is running!');
 });
 
-app.get('/duas', (req, res) => {
-  db.all('SELECT * FROM dua', [], (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json(rows);
-  });
-});
+// app.get('/duas', (req, res) => {
+//   db.all('SELECT * FROM dua', [], (err, rows) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+//     res.json(rows);
+//   });
+// });
 
-app.get('/categories/:categoryId', (req, res) => {
-  const categoryId = req.params.categoryId;
-  db.all('SELECT * FROM dua WHERE category_id = ?', [categoryId], (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json(rows);
-  });
-});
+// app.get('/categories/:categoryId', (req, res) => {
+//   const categoryId = req.params.categoryId;
+//   db.all('SELECT * FROM dua WHERE category_id = ?', [categoryId], (err, rows) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+//     res.json(rows);
+//   });
+// });
 
 // Start server
 app.listen(PORT, () => {
